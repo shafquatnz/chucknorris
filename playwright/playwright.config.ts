6382b098bb22bb171e784df13859257760d6beb1
Shafquat Kabir <shafquat.kabir@dia.govt.nz>
2023-10-31 09:04:32 +0000

    circleci config wip 5

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['junit', { outputFile: 'results.xml' }]
+  ],
   use: {
     trace: 'on-first-retry',
   },
